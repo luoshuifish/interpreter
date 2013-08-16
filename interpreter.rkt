a65212055891f8291c6e85ab1227e0b08a3f6877
yuyongjiang <yuyongjianghit@163.com>
2013-08-16 16:11:18 +0800

    second

--- interpreter.rkt
+++ interpreter.rkt
@@ -4,4 +4,16 @@
 ;;school:Buaa
 ;;Email:yuyongjianghit@163.com
 
-(define (evaluate exp env) ...)
+(define (atom? exp)
+  (not (pair? exp)))
+
+(define (evaluate exp env)
+  (if (atom? exp)
+      (cond ((symbol? exp) (lookup exp env))
+            ((or (number? e) (string? e) (char? e) (boolean? e) (vector? e)) e)
+            (else (display "wrong" e)
+                  ))
+      (case (car exp)
+        ...
+        (else ...)))
+      )
